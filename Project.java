c755e8995fd900bce99b48cc77cfebcdfb8930b3
t00178633 <Liam.McGovern@students.ittralee.ie>
2016-11-17 10:50:20 +0000

    Add files via upload
    In this part, i have created the gui element of my project and I have created a menu, whcih has the headings Customers,Parts, Repairs and Exit

--- Project.java
+++ Project.java
@@ -3,6 +3,8 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.text.*;
+import java.util.*;
 
 public class Project extends JFrame implements ActionListener
 {
@@ -20,5 +22,35 @@ public class Project extends JFrame implements ActionListener
 	
 	public Project()
 	{
-
-    }
+		JLabel opening;
+	    Date now;
+     	now = new Date();
+        SimpleDateFormat today = new SimpleDateFormat();
+		
+		setTitle("Garage Repairs");
+		setSize(300,500);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		Container mainbackground = getmainbackground();
+        mainbackground.setBackground (Color.gray);
+        mainbackground.setLayout(null);
+        
+        createCustomers();
+        createParts();
+        createRepairs();
+        createExit();
+        
+        JMenuBar menuTop = new JMenuBar();
+        setJMenuBar(menuTop);
+        menuTop.setBackground(Color.orange);
+        menuTop.add(Customers);
+        menuTop.add(Parts);
+        menuTop.add(Repairs);
+        menuTop.add(Exit);
+        
+        opening = new JLabel("Hello, Welcome to Garage Repairs." + today.format(now));
+        opening.setSize(250, 50); 
+        mainbackground.add(opening);
+	}
+	
+	  
+}
