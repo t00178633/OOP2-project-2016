b95d1d021749c855a742be2201473470de5ddac5
t00178633 <Liam.McGovern@students.ittralee.ie>
2016-11-29 15:14:49 +0000

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,16 @@
+/*This class is the Customer class, from here, the code is entered to add a new customer, view an registered customer, 
+ *update a current customers details and view a current customer*/
+public class Customer {
+
+	private String addCustomer;
+	private String removeCustomer;
+	private String updateCustomer;
+	private String viewCustomer;
+	
+	public Customer()
+	 
+   
+    
+    
+    
+}
---
+++ t00178633.java
@@ -0,0 +1,5 @@
+//t00178633.java
+/*Java Project*/
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
