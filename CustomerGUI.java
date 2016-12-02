8f07e5c8f8081f4edb8cfb89b8e1d36c978e34e6
t00178633 <Liam.McGovern@students.ittralee.ie>
2016-12-02 16:17:34 +0000

    Add files via upload
    Customer GUI updated

--- CustomerGUI.java
+++ CustomerGUI.java
@@ -5,9 +5,8 @@ import java.text.*;
 import java.util.*;
 import java.io.*;
 
-public class CustomerGUI extends JFrame {
+public class CustomerGUI extends JFrame implements ActionListener{
   private JPanel customer;
-  private JFrame frame;
   private JTextField forenamefield, surnamefield, addressfield, telephonefield;
   private JLabel forename,surname,address,telephone;
   private JButton register;
@@ -15,97 +14,96 @@ public class CustomerGUI extends JFrame {
     
    
     
-   public static void AddCustomer(ArrayList<Customers> custs)
-   { 
-  // AddCustomer = new AddCustomer();
-  //  AddCustomer.build();
-    
-    	frame = new JFrame("Customers ");
- 		frame.getContentPane().setBackground(Color.gray);
- 		frame.setSize(500,300);
- 		frame.setLocation(500,300);
- 		frame.setLayout(null);
- 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+   public CustomerGUI()
+   {    
+        
+ 		setTitle("Customer Details");
+ 		setSize(500,300);
+ 		setLocation(500,300);
  		
-    	
-    	//customer = new JPanel();
-    	//customer.setBackground(Color.gray);
-    	
-    	//register = new JButton("Register");
-    	//forename = new JLabel("Forename");
-    	//surname =  new JLabel ("Surname");
-    	//address =  new JLabel("Address");
-    	//telephone = new JLabel("Telephone");
-    	
+ 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+ 		
+ 
+
+    	Container pane= getContentPane();
+    	pane.setBackground(Color.gray);
+ 		setLayout(new FlowLayout());
         forename = new JLabel("Enter Customer's Forename:");
  	    forename.setSize(30,10);
- 	    forename.setLocation(10,10);
+ 	    forename.setLocation(10,110);
+ 	    pane.add(forename);
  	    
- 	    forenamefield = new JTextField();
+ 	    forenamefield = new JTextField(50);
  	    forenamefield.setColumns(15);
  	    forenamefield.setSize(150,20);
- 	    frame.add(forenamefield);
- 	    forenamefield.setLocation(110,10);
+ 	    add(forenamefield);
+ 	    forenamefield.setLocation(110,210);
  
    
    	    surname = new JLabel("Enter Customer's Surname:");
  	    surname.setSize(300,100);
- 	    surname.setLocation(10,30);
+ 	    surname.setLocation(20,110);
+ 	    pane.add(surname);
  	
  	    surnamefield = new JTextField();
  	    surnamefield.setColumns(15);
- 	    surnamefield.setSize(150,20);
- 	    frame.add(surnamefield);
+ 	    surnamefield.setSize(150,40);
+ 	    add(surnamefield);
  	    surnamefield.setLocation(110,40);
- 
+      
         address = new JLabel("Enter Customer's Address:");
  	    address.setSize(30,10);
  	    address.setLocation(10,10);
+ 	    pane.add(address);
  	
  	    addressfield = new JTextField();
  	    addressfield.setColumns(15);
  	    addressfield.setSize(150,60);
- 	    frame.add(addressfield);
+ 	    add(addressfield);
  	    addressfield.setLocation(110,70);
     
     
         telephone = new JLabel("Enter Customer's Telephone:");
  	    telephone.setSize(30,10);
  	    telephone.setLocation(10,10);
- 	
+ 		pane.add(telephone);
+ 		
  	    telephonefield = new JTextField();
  	    telephonefield.setColumns(15);
- 	    telephonefield.setSize(150,20);
- 	    frame.add(telephonefield);
- 	    telephonefield.setLocation(110,140);
+ 	    telephonefield.setSize(150,100);
+ 	    add(telephonefield);
+ 	    telephonefield.setLocation(110,180);
     
         register = new JButton("Register");
         register.setSize(95,40);
-        register.setLocation(140,180);
-        //put listener on button
+        register.setLocation(340,180);
+  
          
-        frame.add(register);
- 
+        
+ 		register.addActionListener(this);
+    	pane.add(register);
+   
     	
-    	frame.add(forename);
-    	frame.add(surname);
-    	frame.add(address);
-    	frame.add(telephone);
-    	frame.add(register);
-    	frame.add(customer);
-    	
-        frame.setVisible(true);
-    	 	
     }
-    
-    //event-handling code here
-    
-    //when button pressed grab all details from text-field
-    //and then create a new Customer object using Customer constructor
-    //and then add the new Custiomer object to array list
-    
- public static void main(String[] args){
- 	new CustomerGUI();
- } 
+
+public void actionPerformed(ActionEvent event) {
+        
+        
+        if (event.getSource()==register)
+        {
+        String forename= forenamefield.getText();
+   		String surname= surnamefield.getText();
+   		String address=addressfield.getText();
+		double telephone=Double.parseDouble(telephonefield.getText());
+		
+		
+		Customer cust= new Customer(forename,surname, address, telephone);
+		Project.customers.add(cust);
+		JOptionPane.showMessageDialog(null,"Customer has been registered");
+	
+        }
+        
+        
+    }
  	 
 }
