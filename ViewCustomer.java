cb1cd290c4a469d7f92177ba0cc7b9ea2192cd37
t00178633 <Liam.McGovern@students.ittralee.ie>
2016-12-02 16:29:04 +0000

    Add files via upload
    This is the viewcustomer class, in here I have written code which accesses the ArrayList set up in the customergui class to access the customers which have been saved to the arraylist

---
+++ ViewCustomer.java
@@ -0,0 +1,106 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.text.*;
+import java.util.*;
+import java.io.*;
+
+public class ViewCustomer extends JFrame implements ActionListener{
+
+  private JPanel viewcustomer;
+  private JTextField forenamefield, surnamefield, addressfield;
+  private JLabel forename,surname,address;
+  private JButton view; 
+  	
+  	public ViewCustomer()
+   {    
+        
+ 		setTitle("Customer Details");
+ 		setSize(500,300);
+ 		setLocation(500,300);
+ 	
+ 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+    
+    	Container pane= getContentPane();	
+    	pane.setBackground(Color.GRAY);
+ 		setLayout(new FlowLayout());
+        forename = new JLabel("Enter Customer's Forename:");
+ 	    forename.setSize(30,10);
+ 	    forename.setLocation(10,110);
+ 	    pane.add(forename);
+ 	    
+ 	    forenamefield = new JTextField(50);
+ 	    forenamefield.setColumns(15);
+ 	    forenamefield.setSize(150,20);
+ 	    add(forenamefield);
+ 	    forenamefield.setLocation(110,210);
+ 
+   
+   	    surname = new JLabel("Enter Customer's Surname:");
+ 	    surname.setSize(300,100);
+ 	    surname.setLocation(20,110);
+ 	    pane.add(surname);
+ 	
+ 	    surnamefield = new JTextField();
+ 	    surnamefield.setColumns(15);
+ 	    surnamefield.setSize(150,40);
+ 	    add(surnamefield);
+ 	    surnamefield.setLocation(110,40);
+      
+        address = new JLabel("Enter Customer's Address:");
+ 	    address.setSize(30,10);
+ 	    address.setLocation(10,10);
+ 	    pane.add(address);
+ 	
+ 	    addressfield = new JTextField();
+ 	    addressfield.setColumns(15);
+ 	    addressfield.setSize(150,60);
+ 	    add(addressfield);
+ 	    addressfield.setLocation(110,70);
+    
+    
+        view = new JButton("View");
+        view.setSize(95,40);
+        view.setLocation(210,180);
+  
+         
+        
+ 		view.addActionListener(this);
+    	pane.add(view);
+   
+    	
+    }	
+	
+	
+	
+	
+	
+	public void actionPerformed(ActionEvent event) {
+        
+        
+        if (event.getSource()==view)
+        {
+        String forename= forenamefield.getText();
+   		String surname= surnamefield.getText();
+   		String address=addressfield.getText();
+   		ArrayList <Customer> customers = Project.customers;	
+   		
+   		JOptionPane.showMessageDialog(null,customers);
+   		for(int i=0;i<customers.size();i++)
+   		{
+   			Customer aCustomer = customers.get(i);
+   			if(forename.equals(aCustomer.getForename()) && surname.equals(aCustomer.getSurname()) && address.equals(aCustomer.getAddress()))
+   			{
+   				JOptionPane.showMessageDialog(null,customers);
+   			}
+   		
+   		}
+   		
+        }
+        
+        
+    }
+
+    
+    
+}
