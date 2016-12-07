647b6710d96812ab31f9bd593b5229826f543743
t00178633 <Liam.McGovern@students.ittralee.ie>
2016-12-07 11:14:24 +0000

    Add files via upload

--- Customer.java
+++ Customer.java
@@ -1,85 +1,104 @@
 /*This class is the Customer class, from here, the code is entered to add a new customer, view an registered customer, 
  *update a current customers details and view a current customer*/
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.text.*;
-import java.util.*;
-import java.io.*;
+
+/**This is an instantiable Customer class.
+ @author Liam McGovern
+ @version 1.0 */ 
+
+//import javax.swing.*;
+//import java.awt.*;
+//import java.awt.event.*;
+//import java.text.*;
+//import java.util.*;
+//import java.io.*;
 
 public class Customer {
-
-//	private String addCustomer;
-//	private String removeCustomer;
-//	private String updateCustomer;
-//	private String viewCustomer;
+    //attributes
 	private String forename;
    	private String surname;
    	private String address;
 	private double telephone;
-
-	 
-	// public void addCustomer(){
-      //	Customer cust = new Customer();
-      	//cust.setforename(JOptionPane.showInputDialog("Forename:"));
-      	//cust.setsurname(JOptionPane.showInputDialog("Surname:"));
-      	//cust.settelephonename(JOptionPane.showInputDialog("Telephone:"));
-      	//cust.add(cust);
-      
+    
+    /** multiple argument constructor method
+	 @param forename the first name of the Customer
+	 @param surname the surname of the Customer
+	 @param address the address of the Customer
+	 @param telephone the telphone number of the Customer */
 	public  Customer( String forename,String surname, String address, double telephone ){
-		forename = "";
-		surname = "";
-		address ="";
-		telephone = telephone;
+	    this.forename = forename;
+		this.surname = surname;
+		this.address = address;
+		this.telephone = telephone;
 	}
-	
+	/** single argument constructor method
+	 @param forename the forename, surname, address and telephone of the Customer */
 	public  Customer( String forename ){
 		forename = forename;
 		surname = surname;
 		address = address;
 		telephone = telephone;
 	}
-	
+	/** single argument constructor method
+	 @param forename the forename, surname, address and telephone of the Customer */
 	public  Customer( ){
 		forename = "Forename";
 		surname = "Surname";
 		address = "Address";
 	}
 	
+	/** mutator method to set the Customers forename
+	 *@param forename the forename of the Customer */
+
 	public void setForename(String forename){
 		forename = forename;
 	}
-	
+
+     /** mutator method to set the Customers surname
+	 *@param surname the surname of the Customer */	
 	public void setSurname(String surname){
 		surname = surname;
 	}
-	
+	/** mutator method to set the Customers address
+	 *@param address the address of the Customer */
 	public void setAddress(String address){
 		address = address;
 	}
 	
+	/** mutator method to set the Customers telephone number
+	 *@param telephone the telephone number of the Customer */
 	public void setTelephone(double telephone){
 		telephone = telephone;
 	}
 	
+	/** acessor method to return the Customer forename
+	 *@return forename the forename of the Customer */
+
 	public String getForename(){
 	  return forename;	
 	}
 	
+	/** acessor method to return the Customer surname
+	 *@return surname the surname of the Customer */
 	public String getSurname(){
 	  return surname;	
 	}
 	
+	/** acessor method to return the Customer address
+	 *@return address the address of the Customer */
 	public String getAddress(){
 		return address;
 	}
 	
+	/** acessor method to return the Customer telephone number
+	 *@return telephone the telephone number of the Customer */
 	public double getTelephone(){
 	  return telephone;	
 	}
-	
+	/** toString method to return the Customer details
+	 *@return forename + " " + surname + " " + address + " " + telephone the forename, surname, address and telephone number of the Customer as a string */
+
 	public String toString() {
-		return forename + " " + surname + " " + address + " " + telephone;
+		return "Forename: " + forename + " " + "\nSurname: " + surname + " " +"\nAddress: " + address + " " + "\nTelephone: " + telephone;
 	}
 
       
@@ -87,7 +106,7 @@ public class Customer {
      {
      	new Customer();
      }
-}	 
+}//end class	 
    
     
     
