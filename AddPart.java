917d7264b3e922288bdb38b35eaf6efe13af0562
t00178633 <Liam.McGovern@students.ittralee.ie>
2016-12-07 11:14:16 +0000

    Add files via upload

--- AddPart.java
+++ AddPart.java
@@ -39,6 +39,8 @@ public class AddPart extends JFrame implements ActionListener{
  	    add(partnamefield);
  	    partnamefield.setLocation(110,210);
  
+        JLabel blankLabel1 = new JLabel("                                             ");
+        pane.add(blankLabel1);
    
    	    partdetails = new JLabel("Enter Part Details:");
  	    partdetails.setSize(300,100);
@@ -50,7 +52,10 @@ public class AddPart extends JFrame implements ActionListener{
  	    partdetailsfield.setSize(150,40);
  	    add(partdetailsfield);
  	    partdetailsfield.setLocation(110,40);
-      
+     
+        JLabel blankLabel2 = new JLabel("                                             ");
+        pane.add(blankLabel2);
+       
         partquantity = new JLabel("Enter Parts Quantity:");
  	    partquantity.setSize(30,10);
  	    partquantity.setLocation(10,10);
